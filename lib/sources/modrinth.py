4fb27507497fa5f5a7e389a052768d7899984b50
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2025-05-18 17:27:05 -0400

    Add multiple mods in add command

--- lib/sources/modrinth.py
+++ lib/sources/modrinth.py
@@ -4,6 +4,7 @@ import requests
 
 API = "https://api.modrinth.com/v2"
 
+
 def search(query: str) -> dict:
     response = requests.get(f"{API}/search", params={"query": query})
     return response.json()
@@ -20,6 +21,7 @@ def get_project(slug: str) -> dict:
 def get_projects(slugs: list[str]) -> dict:
     formatted_slugs = str(slugs).replace("'", '"')  # API requires double-quotes
     return requests.get(f"{API}/projects", params={"ids": formatted_slugs}).json()
+
 
 def get_versions(slug: str) -> dict:
     return requests.get(f"{API}/project/{slug}/version").json()
