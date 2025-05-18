2ba334ab9e3a78c2f7f56d63201024b57611701f
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2025-05-18 17:02:12 -0400

    Improve toml lib + cli. Basically works like uv now

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import sys
 from pprint import pprint
 import argparse
 
-
 def search(args: argparse.Namespace):
     results = modrinth.search(args.query)
     print("Results:")
@@ -20,6 +19,15 @@ def info(args: argparse.Namespace):
 
 def toml(args: argparse.Namespace):
     mcproject.init_mcproject_toml(args.path, args.force)
+
+def add(args: argparse.Namespace):
+    try:
+        toml = mcproject.read_mcproject_toml(args.path)
+    except FileNotFoundError:
+        toml = mcproject.read_mcproject_toml(mcproject.init_mcproject_toml(args.path))
+
+    mcproject.add_dependency(toml, args.mod)
+    mcproject.write_mcproject_toml(toml, args.path)
 
 
 def create_parser() -> argparse.ArgumentParser:
@@ -39,12 +47,20 @@ def create_parser() -> argparse.ArgumentParser:
     toml_cmd = commands.add_parser(
         "toml", description="Make a new toml file (testing only)."
     )
-    toml_cmd.add_argument("path", type=Path)
+    toml_cmd.add_argument("path", type=Path, nargs="?", default=Path("mcproject.toml"))
     toml_cmd.add_argument(
         "--force", action="store_true", help="Overwrite the file if it already exists."
     )
     toml_cmd.set_defaults(func=toml)
 
+    add_cmd = commands.add_parser(
+        "add", description="Add a mod to mcproject.toml."
+    )
+    add_cmd.add_argument("path", type=Path, nargs="?", default=Path("mcproject.toml"))
+    add_cmd.add_argument(
+        "mod", help="Mod to add to mcproject.toml."
+    )
+    add_cmd.set_defaults(func=add)
     return parser
 
 
