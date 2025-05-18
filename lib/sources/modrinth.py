cb023931b7d7b7197213934d5385141b3f869b82
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2025-05-18 12:41:07 -0400

    modrinth: get project(s) info

--- lib/sources/modrinth.py
+++ lib/sources/modrinth.py
@@ -1,9 +1,20 @@
 import requests
 
-API = "https://api.modrinth.com"
-SEARCH = API + "/v2/search"
+API = "https://api.modrinth.com/v2/"
+SEARCH = API + "search"
+PROJECT = API + "project"
+PROJECTS = API + "projects"
 
 
-def search(query: str):
+def search(query: str) -> dict:
     response = requests.get(SEARCH, params={"query": query})
     return response.json()
+
+
+def get_project(slug: str) -> dict:
+    return requests.get(f"{PROJECT}/{slug}").json()
+
+
+def get_projects(slugs: list[str]) -> dict:
+    formatted_slugs = str(slugs).replace("'", '"')  # API requires double-quotes
+    return requests.get(PROJECTS, params={"ids": formatted_slugs}).json()
