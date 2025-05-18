f0805b15ba3f969a6494ff33cd7bac91da86abcc
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2025-05-18 12:42:20 -0400

    simple cli

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from lib.sources import modrinth
+import argparse
 
 
-def main():
-    print(modrinth.search("gravestones"))
+def search(args: argparse.Namespace):
+    results = modrinth.search(args.query)
+    print("Results:")
+    for result in results["hits"]:
+        print(f"{result["title"]} ({result["slug"]})")
+
+
+def parse_args(args: list[str] | None = None) -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    search_group = parser.add_argument_group("search", description="Search for a mod.")
+    # search_group.add_argument("--from", help="Specify a source (e.g., Modrinth, Curseforge)")
+    search_group.add_argument("query", help="Query to search the source for.")
+    search_group.set_defaults(func=search)
+    return parser.parse_args(args)
+
+
+def cli():
+    args = parse_args()
+    args.func(args)
 
 
 if __name__ == "__main__":
-    main()
+    cli()
