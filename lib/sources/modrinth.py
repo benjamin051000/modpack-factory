6a9ad8a1a47b66fdd0b486ce915dbf09f4975f5e
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2025-06-06 21:29:15 -0400

    Filter out unsupported game versions.
    Right now, the only supported game version is release.
    
    The following are filtered out:
    - alpha
    - beta
    - pre-release
    - release candidates
    - snapshots
    - pre-classic (`rd-`)
    - classic (c, I think?)
    - indev/infdev
    
    For more info, see https://minecraft.wiki/w/Version_formats

--- lib/sources/modrinth.py
+++ lib/sources/modrinth.py
@@ -7,7 +7,8 @@ import requests
 
 API = "https://api.modrinth.com/v2/"
 
-# TODO make this into a subclass so we can dispatch to the appropriate one once curseforge is added.
+# TODO make this into a subclass so we can dispatch to
+# the appropriate one once curseforge is added.
 
 
 def search(query: str) -> dict:
@@ -30,9 +31,27 @@ def get_projects(slugs: list[str]) -> list[dict]:
     return response.json()
 
 
-async def get_versions(session: aiohttp.ClientSession, slug: str) -> dict:
+async def get_versions(session: aiohttp.ClientSession, slug: str) -> list:
     async with session.get(f"project/{slug}/version") as response:
-        return await response.json()
+        versions_json = await response.json()
+
+    # Filter out versions which only run on a Minecraft snapshot.
+    # Remove snapshots for now.
+    for version in versions_json:
+        version["game_versions"] = [
+            v
+            for v in version["game_versions"]
+            # Skip snapshots, pre-releases, RCs, beta versions.
+            # Sourced from modrith web filter
+            if not any(
+                c in v for c in ["w", "-pre", "-rc", "b", "a", "c", "rd-", "inf-"]
+            )
+        ]
+
+    # Versions which only supported snapshots now have empty game_versions fields.
+    filtered_versions_json = [v for v in versions_json if len(v["game_versions"])]
+
+    return filtered_versions_json
 
 
 def download_jar(url: str, filename: Path):
