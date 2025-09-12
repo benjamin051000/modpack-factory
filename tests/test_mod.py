f6237b50f4c23c63416695206f69bd4d08533495
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2025-09-12 15:00:46 -0400

    test_batched_one_dependency now works, but at what cost?

--- tests/test_mod.py
+++ tests/test_mod.py
@@ -49,5 +49,58 @@ async def test_from_batched_one_dependency(modrinth: Modrinth):
     }
 
     reeses_sodium_options = mods[0]
+    assert reeses_sodium_options.slug == "reeses-sodium-options"
+
+    # These versions have no dependencies listed on Modrinth,
+    # even though they definitely do... Oops!
+    quirky_versions = [
+        "mc1.17.1-1.4.4",
+        "mc1.16.5-1.4.7",
+        "mc1.18.2-1.4.6",
+        "mc1.16.5-1.4.1",
+        "mc1.17.1-1.4.1",
+        "mc1.18-1.2.3",
+        "mc1.17.1-1.4.5",
+        "mc1.18.2-1.4.5",
+        "mc1.18.1-1.3.0",
+        "mc1.18.1-1.4.0",
+        "mc1.16.5-1.4.6",
+        "mc1.18.2-1.4.2",
+        "mc1.18.2-1.4.3",
+        "mc1.17.1-1.3.0",
+        "mc1.18.2-1.4.7",
+        "mc1.16.5-1.4.5",
+        "mc1.17.1-1.4.3",
+        "1.2.1",
+        "mc1.18.2-1.4.4",
+        "mc1.17.1-1.4.6",
+        "mc1.16.5-1.3.0",
+        "mc1.16.5-1.4.2",
+        "mc1.16.5-1.4.3",
+        "mc1.18.2-1.4.1",
+        "mc1.18.1-1.2.4",
+        "mc1.19-1.4.3",
+        "mc1.19.2-1.4.6",
+        "mc1.19.2-1.4.7",
+        "mc1.16.5-1.4.4",
+        "mc1.17.1-1.2.3",
+        "mc1.16.5-1.4.0",
+        "mc1.19-1.4.4",
+        "mc1.17.1-1.4.7",
+        "mc1.17.1-1.4.2",
+        "mc1.17.1-1.2.2",
+        "mc1.17.1-1.4.0",
+        "1.2.0",
+        "mc1.19.2-1.4.5",
+        "25",
+    ]
+
+    for version in reeses_sodium_options.versions:
+        if version.version_number not in quirky_versions:
+            assert len(version.dependencies) == 1
+            # Horrible!
+            dep = next(iter(version.dependencies.values())).pop()
+            assert dep.mod_slug == "sodium"
 
     # Test case: mc1.17.1-1.4.4 has 0 deps
+    # So does mc1.16.5-1.4.7
