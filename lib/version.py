c799c69f1d3b3cabb40c27fe9af97b2c1eaf07ec
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2026-01-02 16:18:13 -0500

    version: Unwrap list of one elem. Allow optional_minor_and_patch.

--- lib/version.py
+++ lib/version.py
@@ -1,62 +1,108 @@
-from operator import ge, gt, le, lt
+from collections.abc import Callable
+from contextlib import suppress
+from operator import eq, ge, gt, le, lt
 from typing import Self
 
 from semver import Version
 
-MAX_VERSION_NUM = 999_999
+OPERATOR_STR_TO_FN: dict[str, Callable] = {">": gt, ">=": ge, "<": lt, "<=": le, "": eq}
 
 
-# NOTE: This cannot be a dataclass unless you're willing to change the variable order.
 class VersionInterval:
-    def __init__(
-        self,
-        min_inclusive: bool,
-        min: Version | None,
-        max: Version | None,
-        max_inclusive: bool,
-    ) -> None:
-        self.min_inclusive = min_inclusive
-        self.min = min if min is not None else Version(0)
-        self.max = (
-            max
-            if max is not None
-            else Version(MAX_VERSION_NUM, MAX_VERSION_NUM, MAX_VERSION_NUM)
-        )
-        self.max_inclusive = max_inclusive
+    """Represents a single version interval, which is an operator and a version.
+
+    Note that versions with ^, ~, or X-ranges (xX*) cannot be represented by a single
+    VersionInterval. They require multiple VersionInterval objects.
+    However, this class will strip the string of these characters for convenience.
+    """
+
+    def __init__(self, operator: str, version: Version, s: str = "") -> None:
+        self.operator = OPERATOR_STR_TO_FN[operator]
+        self.version = version
+        self._s = s
 
     def __str__(self) -> str:
-        front = "[" if self.min_inclusive else "("
-        back = "]" if self.max_inclusive else ")"
-        return f"{front}{self.min}, {self.max}{back}"
+        if self._s:
+            return self._s
+
+        return f"{self.operator.__name__}{self.version}"
 
     def __repr__(self) -> str:
-        return str(self)
+        return (
+            f"VersionInterval({self.operator.__name__}, {self.version!r}, {self._s!r})"
+        )
+
+    def __eq__(self, other: object, /) -> bool:
+        return (
+            isinstance(other, VersionInterval)
+            and self.operator == other.operator
+            and self.version == other.version
+        )
+
+    def __hash__(self) -> int:
+        return hash((self.operator, self.version))
 
     @classmethod
-    def from_str(cls, s: str) -> Self:
-        if s == "*":  # Match all versions.
-            return cls(True, None, None, True)
-        elif s.startswith(">="):
-            return cls(
-                True, Version.parse(s[2:], optional_minor_and_patch=True), None, True
-            )
-        elif s.startswith("<="):
-            return cls(
-                True, None, Version.parse(s[2:], optional_minor_and_patch=True), True
-            )
-        elif s.startswith(">"):
-            return cls(
-                False, Version.parse(s[1:], optional_minor_and_patch=True), None, True
-            )
-        elif s.startswith("<"):
-            return cls(
-                True, None, Version.parse(s[1:], optional_minor_and_patch=True), False
-            )
-        else:
-            raise ValueError(f"Invalid version constraint: {s}")
+    def from_str(cls, s: str) -> list[Self]:
+        """
+        Given the version range string, produce a list of VersionIntervals
+        that represent it.
+        """
+        # Match all versions.
+        if s == "*":
+            return [cls(">=", Version(0, 0, 0), s)]
+
+        # The default case, which covers <, >, ^, ~
+        operator = s[0]
+        version = s[1:]
+
+        # Check if it's <= or >=
+        if operator in {"<", ">"} and s[1] == "=":
+            operator = s[:2]
+            version = s[2:]
+
+        # Check if it starts with the number
+        elif s[0] in [str(s) for s in range(10)]:
+            operator = ""
+            version = s
+
+        # Handle ^ and ~, which create two VersionIntervals.
+        # I *think* that this cannot be combined with X-ranges.
+        if operator == "^":
+            low = Version.parse(version, optional_minor_and_patch=True)
+            high = low.bump_major()
+            return [cls(">=", low), cls("<", high)]
+        elif operator == "~":
+            low = Version.parse(version, optional_minor_and_patch=True)
+            high = low.bump_minor()
+            return [cls(">=", low), cls("<", high)]
+
+        # Handle X-ranges in the minor or patch.
+        # TODO "Additional trailing .x/.X/.* are allowed but have no effect"
+        X_VALS = ["x", "X", "*"]
+
+        if any(version.endswith(c) for c in X_VALS):
+            version_no_x = version.replace(".x", "").replace(".X", "").replace(".*", "")
+            low = Version.parse(version_no_x, optional_minor_and_patch=True)
+            for x in X_VALS:
+                with suppress(ValueError):
+                    x_idx = version.split(".").index(x)
+                    break
+            else:
+                print("Something weird happened 1")
+                exit(1)
+
+            if x_idx == 1:
+                high = low.bump_major()
+            elif x_idx == 2:
+                high = low.bump_minor()
+            else:
+                print("Something weird happened 2")
+                exit(1)
+            return [cls(">=", low), cls("<", high)]
+
+        return [cls(operator, Version.parse(version, optional_minor_and_patch=True), s)]
 
     def __contains__(self, version: Version) -> bool:
-        """Whether the interval contains version."""
-        min_op = ge if self.min_inclusive else gt  # above the min
-        max_op = le if self.max_inclusive else lt  # below the max
-        return min_op(version, self.min) and max_op(version, self.max)
+        """Whether the interval contains a version."""
+        return self.operator(version, self.version)
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,34 +1,115 @@
 import pytest
 
-from lib.jar import Constraint
+from lib.jar import FabricVersionRange
 from lib.version import Version, VersionInterval
 
 
 @pytest.mark.parametrize(
     "s, contains_0_9_0, contains_1_0_0, contains_2_0_0",
     [
+        ("<=0.1.0", False, False, False),
         (">1.0.0", False, False, True),
         (">=1.0.0", False, True, True),
         ("<1.0.0", True, False, False),
         ("<=1.0.0", True, True, False),
+        ("1.0.0", False, True, False),
+        (">=0.0.0", True, True, True),
     ],
 )
-def test_version_interval_contains(
+def test_single_version_interval(
     s: str, contains_0_9_0: bool, contains_1_0_0: bool, contains_2_0_0: bool
 ):
-    vi = VersionInterval.from_str(s)
+    """Test single (one-ended) VersionIntervals."""
+    vis = VersionInterval.from_str(s)
+
+    assert len(vis) == 1
+    vi = vis[0]
 
     assert (Version.parse("0.9.0") in vi) == contains_0_9_0
     assert (Version.parse("1.0.0") in vi) == contains_1_0_0
     assert (Version.parse("2.0.0") in vi) == contains_2_0_0
 
 
-def test_version_interval_comparison():
-    vi = VersionInterval.from_str(">1.0.0")
-    assert Version.parse("1.1.0") in vi
-
-
 def test_version_constraint_comparison():
     vi = VersionInterval.from_str(">1.0.0")
-    c = Constraint("sodium", vi)
+    c = FabricVersionRange("sodium", vi)
     assert Version.parse("1.1.0") in c
+
+
+def test_version_carat():
+    s = "^1.0.0"
+    correct = {
+        VersionInterval(">=", Version(1, 0, 0)),
+        VersionInterval("<", Version(2, 0, 0)),
+    }
+    vis = set(VersionInterval.from_str(s))
+
+    assert vis == correct
+
+
+def test_version_tilde():
+    s = "~1.0.0"
+    correct = {
+        VersionInterval(">=", Version(1, 0, 0)),
+        VersionInterval("<", Version(1, 1, 0)),
+    }
+    vis = set(VersionInterval.from_str(s))
+
+    assert vis == correct
+
+
+@pytest.mark.parametrize(
+    "s",
+    [
+        "1.0.x",
+        "1.0.X",
+        "1.0.*",
+    ],
+)
+def test_version_x_range_patch(s: str):
+    vis = set(VersionInterval.from_str(s))
+    correct = {
+        VersionInterval(">=", Version(1, 0, 0)),
+        VersionInterval("<", Version(1, 1, 0)),
+    }
+    assert vis == correct
+
+
+@pytest.mark.parametrize(
+    "s",
+    [
+        "1.x",
+        "1.X",
+        "1.*",
+    ],
+)
+def test_version_x_range_minor(s: str):
+    vis = set(VersionInterval.from_str(s))
+    correct = {
+        VersionInterval(">=", Version(1, 0, 0)),
+        VersionInterval("<", Version(2, 0, 0)),
+    }
+    assert vis == correct
+
+
+@pytest.mark.parametrize(
+    "s",
+    [
+        "1.x.x",
+        "1.x.X",
+        "1.x.*",
+        "1.X.x",
+        "1.X.X",
+        "1.X.*",
+        "1.*.x",
+        "1.*.X",
+        "1.*.*",
+    ],
+)
+def test_version_x_range_minor_double_x(s: str):
+    vis = set(VersionInterval.from_str(s))
+    correct = {
+        VersionInterval(">=", Version(1, 0, 0)),
+        VersionInterval("<", Version(2, 0, 0)),
+    }
+    assert vis == correct
