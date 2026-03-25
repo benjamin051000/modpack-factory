16901ad2eec1678171af881b536e7facdff88410
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2026-03-24 21:15:41 -0400

    test_mod.py passes! But...
    I'm pretty sure the datatype for Mod().depends is not going to work for
    us. Right now it's slug -> list of version_json, I think? We may need to
    do a second pass before exiting the constructor function to replace them
    all with refs to their Mod objects. Should actually be quite easy
    because you can just look up the relevant mod obj. But still annoying.
    Probably the right move for a first attempt though, as I want to get rid
    of as much JSON as possible.
    
    Signed-off-by: Benjamin Wheeler <benjaminwheeler0510@gmail.com>

--- tests/test_mod.py
+++ tests/test_mod.py
@@ -60,7 +60,7 @@ async def test_from_batched_simple(modrinth: Modrinth):
     constraints = await FabricJarConstraints.from_modrinth_batched(
         modrinth, versions_json
     )
-    mods = Mod.from_batched(modrinth, mods_json, versions_json, constraints)
+    mods = Mod.from_batched(mods_json, versions_json, constraints)
 
     # There should be at least one Mod per slug. Likely far more.
     assert len(mods) >= len(slugs)
@@ -79,7 +79,7 @@ async def test_from_batched_one_dependency(modrinth: Modrinth):
     constraints = await FabricJarConstraints.from_modrinth_batched(
         modrinth, versions_json
     )
-    mods = Mod.from_batched(modrinth, mods_json, versions_json, constraints)
+    mods = Mod.from_batched(mods_json, versions_json, constraints)
 
     assert {mod.slug for mod in mods} == {
         "reeses-sodium-options",
@@ -89,56 +89,10 @@ async def test_from_batched_one_dependency(modrinth: Modrinth):
     reeses_sodium_options = mods[0]
     assert reeses_sodium_options.slug == "reeses-sodium-options"
 
-    # These versions have no dependencies listed on Modrinth,
-    # even though they definitely do... Oops!
-    quirky_versions = [
-        "mc1.17.1-1.4.4",
-        "mc1.16.5-1.4.7",
-        "mc1.18.2-1.4.6",
-        "mc1.16.5-1.4.1",
-        "mc1.17.1-1.4.1",
-        "mc1.18-1.2.3",
-        "mc1.17.1-1.4.5",
-        "mc1.18.2-1.4.5",
-        "mc1.18.1-1.3.0",
-        "mc1.18.1-1.4.0",
-        "mc1.16.5-1.4.6",
-        "mc1.18.2-1.4.2",
-        "mc1.18.2-1.4.3",
-        "mc1.17.1-1.3.0",
-        "mc1.18.2-1.4.7",
-        "mc1.16.5-1.4.5",
-        "mc1.17.1-1.4.3",
-        "1.2.1",
-        "mc1.18.2-1.4.4",
-        "mc1.17.1-1.4.6",
-        "mc1.16.5-1.3.0",
-        "mc1.16.5-1.4.2",
-        "mc1.16.5-1.4.3",
-        "mc1.18.2-1.4.1",
-        "mc1.18.1-1.2.4",
-        "mc1.19-1.4.3",
-        "mc1.19.2-1.4.6",
-        "mc1.19.2-1.4.7",
-        "mc1.16.5-1.4.4",
-        "mc1.17.1-1.2.3",
-        "mc1.16.5-1.4.0",
-        "mc1.19-1.4.4",
-        "mc1.17.1-1.4.7",
-        "mc1.17.1-1.4.2",
-        "mc1.17.1-1.2.2",
-        "mc1.17.1-1.4.0",
-        "1.2.0",
-        "mc1.19.2-1.4.5",
-        "25",
-    ]
-
-    for version in reeses_sodium_options.versions:
-        if version.version_number not in quirky_versions:
-            assert len(version.dependencies) == 1
-            # Horrible!
-            dep = next(iter(version.dependencies.values())).pop()
-            assert dep.mod_slug == "sodium"
+    for mod in mods:
+        if mod.slug == "reeses-sodium-options":
+            assert len(mod.depends) == 1
+            assert set(mod.depends.keys()) == {"sodium"}
 
     # Test case: mc1.17.1-1.4.4 has 0 deps
     # So does mc1.16.5-1.4.7
